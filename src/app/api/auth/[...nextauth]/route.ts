8f447a0e4c2c63b06e90ac2c48911acccff54563
Neelam Gauchan <gauchanneelam@gmail.com>
2025-06-12 18:10:15 -0400

    Added Google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 
 declare module "next-auth" {
@@ -30,6 +31,11 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  
   ],
   callbacks: {
     async jwt({ token, account }) {
